fa408fb60241578e5ca5288d8e380673f4a28f20
pujh <pujh@idste.cn>
2023-10-12 23:43:47 +0800

    remove build

--- Test/build/generated/ap_generated_sources/debug/out/io/realm/com_imooc_viewpagertest_module_search_SearchModelRealmProxyInterface.java
+++
@@ -1,9 +0,0 @@
-package io.realm;
-
-
-public interface com_imooc_viewpagertest_module_search_SearchModelRealmProxyInterface {
-    public long realmGet$uptime();
-    public void realmSet$uptime(long value);
-    public RealmList<com.imooc.viewpagertest.module.search.ProductModel> realmGet$list();
-    public void realmSet$list(RealmList<com.imooc.viewpagertest.module.search.ProductModel> value);
-}
